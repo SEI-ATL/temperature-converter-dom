85c46bd24bcd45496e945b1213fcc9f7bd177d68
jtreeves <jackson.t.reeves@gmail.com>
2020-10-22 17:02:14 -0400

    [initiated] Project initiated

--- js/script.js
+++ js/script.js
@@ -1,1 +1,7 @@
-console.log('Hello, front end');
+function convertToCelsius(Fahrenheit) {
+    return (5/9) * (Fahrenheit - 32);
+}
+
+function convertToFahrenheit(Celsius) {
+    return (9/5) * Celsius + 32;
+}
