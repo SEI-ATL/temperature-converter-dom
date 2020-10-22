cc86692465866d95d51e057d650196dcb8f7b9e5
delayedaa <delayedaa@gmail.com>
2020-10-22 18:38:36 -0500

    [script.js] Build initial converter site

--- js/script.js
+++ js/script.js
@@ -1,1 +1,43 @@
-console.log('Hello, front end');
+
+let submitButton = document.querySelector('.submit');
+let clearButton = document.querySelector('.clear');
+let inputField = document.querySelector('#input');
+let h2 = document.querySelector('h2');
+let outputDiv = document.querySelector('.output');
+let fButton = document.querySelector('.f');
+let cButton = document.querySelector('.c');
+
+
+
+
+
+submitButton.addEventListener('click', function() {
+  debugger;
+  let inputValue = inputField.value;
+  let output;
+  if (fButton.checked === true) {
+    output = (inputValue - 32) * (5/9);
+    output = output.toFixed(2);
+    h2.innerHTML = output + '&deg;C';
+    if (output > 0) {
+      outputDiv.classList.add('hot');
+    } else {
+      outputDiv.classList.add('cold');
+    }
+  } else {
+    output = (inputValue * 1.8) + 32;
+    output = output.toFixed(2);
+    h2.innerHTML = output + '&deg;F';
+    if (output > 32) {
+      outputDiv.classList.add('hot');
+    } else {
+      outputDiv.classList.add('cold');
+    }
+  }
+});
+
+clearButton.addEventListener('click', function() {
+  inputField.value = '';
+  h2.innerHTML = '';
+  outputDiv.className = 'output';
+});
