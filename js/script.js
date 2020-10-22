c94c7778572e78ac2826936f609a0fbe7c12ed85
Nicole Hamilton <Nicole@Warps-MacBook-Air.local>
2020-10-22 15:53:34 -0700

    [js] fixed const output code

--- js/script.js
+++ js/script.js
@@ -1,11 +1,12 @@
-const fahrenheit = document.querySelector('#input').value
-// document.querySelector('#here').value What I used to try to take the place of 0
-const celsius = 0
+// const fahrenheit = document.querySelector('#input').value
 
-function fahToCel() {
-    cel = ((fahrenheit - 32)/1.8)
-    return cel
-}
+// // document.querySelector('#here').value What I used to try to take the place of 0
+// const celsius = 0
+
+// function fahToCel() {
+//     cel = ((fahrenheit - 32)/1.8)
+//     return cel
+// }
 
 
 // function celToFah() {
@@ -14,12 +15,15 @@ function fahToCel() {
 // }
 
 
-document.querySelector('#submit').addEventListener('click', function() {
-    cel = ((fahrenheit - 32)/1.8)
-    console.log(cel)
+const output =document.querySelector('#submit').addEventListener('click', function() {
+    const fahrenheit = document.querySelector('#input').value
+    console.log(fahrenheit);
+    let cel = ((fahrenheit - 32)/1.8)
+    console.log(cel);
     return cel
+    
 })
-
+console.log(output);
 
 //on button click run function fahToCel grabbing info from input GOAL
 //clear Button should set the output to 0 or null
