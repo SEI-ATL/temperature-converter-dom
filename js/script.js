b19e4ffaeb1c69c6dc1d927f4626d11c191b4fd0
Your Name <you@example.com>
2020-10-22 18:09:37 -0400

    [script.js] submit button done

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
-const fahrenheit = 0
+const fahrenheit = document.querySelector('#input').value
+// document.querySelector('#here').value What I used to try to take the place of 0
 const celsius = 0
 
 function fahToCel() {
@@ -12,4 +13,13 @@ function fahToCel() {
 //     return fah
 // }
 
-document.querySelector('#input')
+
+document.querySelector('#submit').addEventListener('click', function() {
+    cel = ((fahrenheit - 32)/1.8)
+    console.log(cel)
+    return cel
+})
+
+
+//on button click run function fahToCel grabbing info from input GOAL
+//clear Button should set the output to 0 or null
