30ab5948a77a6e187a187283c46c4844c0e9aa06
Edree Huss <edreeshuss@gmail.com>
2020-10-23 01:46:45 -0400

    [TempConverter] Create temperature converter

--- js/script.js
+++ js/script.js
@@ -1,1 +1,24 @@
-console.log('Hello, front end');
+var temperatures = function(t) {
+        var output, given, result, opposite;
+        given = document.getElementById('temp').value;
+        if (t === "C") {
+            result = 1.8 * (+given) + 32;
+            opposite = "F";
+        } else {
+            result = (given - 32) / 1.8;
+            opposite = "C";
+        }
+        if (result % 1 !== 0) {
+            console.log(result % 1);
+            result = result.toFixed(2);
+        }
+        if (given || given === "0") {
+            output = given + " \u00b0" + t + " is equal to " + result + " \u00b0" + opposite;
+        } else {
+            output = "Please enter a number";
+        }
+        document.querySelector('output').innerHTML = output;
+    }
+document.getElementById('ctof').addEventListener('click', function(){temperatures('C');})
+document.getElementById('ctof2').addEventListener('click', function(){temperatures('F');})
+
