556d04c43b771f1411727037762b2ef4b5f029aa
Felix Muwanguzi <felixm@Felixs-MacBook-Air.local>
2020-10-22 23:13:36 -0400

    [still working on the equation

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
-console.log('Hello, front end');
+const output = document.querySelctor('#Temp').addEventListener('click', function(){
+    let answer= document.querySelctor('#input').value;
+
+    console.log(output);
+
+    let answer = ((output-32)/1.8)
+    console.log(output);
+}
