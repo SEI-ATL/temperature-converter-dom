54745377d81be9b98118e185b9b34a8b811df1a5
Ruben Cedeno <rubencedeno@rubens-mbp.myfiosgateway.com>
2020-10-22 20:24:48 -0400

    [All Files]Update all files

--- js/script.js
+++ js/script.js
@@ -1,1 +1,26 @@
-console.log('Hello, front end');
+
+function farToCel(){ 
+    const inputVal = document.querySelector('#txtbox').value;
+        if (inputVal > 1 || inputVal < 999){
+            var newVal = (inputVal - 32) / 1.8;
+    document.getElementById('screen').innerHTML = Math.floor(newVal) + '°';
+    } else {
+        document.getElementById('screen').innerHTML = 'PLEASE ENTER A NUMBER';
+    }
+}
+
+function celToFar(){ 
+    const inputVal = document.querySelector('#txtbox').value;
+        if (inputVal > 1 || inputVal < 999){
+            var newVal = inputVal * 9 / 5 + 32;
+    document.getElementById('screen').innerHTML = Math.floor(newVal) + '°';
+    } else {
+        document.getElementById('screen').innerHTML = 'PLEASE ENTER A NUMBER';
+    }
+}
+
+
+function resetBox(){
+    const noVal = document.querySelector('#txtbox').value=' ';
+}
+
