80dee7265138ec42b60e3a19040dc90a24b79c7d
jtreeves <jackson.t.reeves@gmail.com>
2020-10-22 23:44:01 -0400

    [js] Creates functions and DOM events

--- js/script.js
+++ js/script.js
@@ -5,3 +5,18 @@ function convertToCelsius(Fahrenheit) {
 function convertToFahrenheit(Celsius) {
     return (9/5) * Celsius + 32;
 }
+
+let input = document.querySelector('#temperature')
+let output = document.querySelector('#converted')
+let selection = document.querySelector('#selection').value
+let conversion = ''
+
+document.querySelector('#submit').addEventListener('click', function(event) {
+    if (selection === 'Celsius') {
+        conversion = convertToCelsius(input);
+    } else {
+        conversion = convertToFahrenheit(input);
+    }
+    output.innerHTML = `<p>${conversion}</p>`;
+    console.log('It ran.');
+})
