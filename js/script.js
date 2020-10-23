061f441b0de5a04425a6a5da5577c93d0303e795
Will Locklier <will.locklier@gmail.com>
2020-10-22 20:38:28 -0400

    [script] Add modified files

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
-console.log('Hello, front end');
+const submitButton = document.getElementById('submitButton');
+submitButton.addEventListener('click', function() {
+    const tempF = document.getElementById('input1');
+    const answer = document.getElementById('answer');
+    answer.innerText = Math.ceil(((tempF.value - 32) * 5) / 9);
+})
+const cancelButton = document.getElementById('cancelButton');
+cancelButton.addEventListener('click',function() {
+    answer.innerText = "Answer cleared. Enter a new value above!";
+})
