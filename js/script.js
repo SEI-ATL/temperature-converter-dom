1d626646e6c3e07c80cfa29abb7562be49d1e57a
Richard Leung <michelleli@MMac.local>
2020-10-23 09:27:41 -0400

    [script.js] correct code

--- js/script.js
+++ js/script.js
@@ -2,36 +2,38 @@ function radioValue() {
     let units = document.getElementsByName('unit');  
     for(i = 0; i < units.length; i++) { 
         if (units[i].checked) {
-        console.log(units[i])
-        return units[i];
+        return units[i].value;
         }
     } 
-} 
+};
 
 function convertToFah() {
-    let num = document.getElementById('temp').value();
+    let num = document.getElementById('temp').value;
     let result = num * 9 / 5 + 32;
     console.log(result);
     document.getElementById('display').innerText = Math.floor(result) + ' ºC';
-}
+};
 
 function convertToCel() {
-    let num = document.getElementById('temp').submit();
-    let results = (num - 32) * 5 / 9;
-    console.log(results);
+    let num = document.getElementById('temp').value;
+    let result = (num - 32) * 5 / 9;
+    console.log(result);
     document.getElementById('display').innerText = Math.floor(result) + ' ºC';
-}
+};
 
-document.querySelector('#submit').addEventListener('click', function() {
+document.querySelector('#submit').addEventListener('click', function(event) {
     unit = radioValue();
+    console.log(document.getElementById('temp').value);
+    console.log(unit);
     convert(unit);
+    debugger;
 });
 
 function convert(unit) {
-    if (unit === 'F') {
+    if (unit === 'C') {
         convertToFah()
     }
-    if (unit === 'C') {
+    if (unit === 'F') {
         convertToCel()
     }
-}
+};
