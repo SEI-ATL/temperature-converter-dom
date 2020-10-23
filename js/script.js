f92084c1074edbe1d59f854d2806ca6ce5ec9ad7
alexjbustillos <alexjbustillos@gmail.com>
2020-10-22 22:10:24 -0400

    [temperature]converter deliverable finished

--- js/script.js
+++ js/script.js
@@ -1,1 +1,30 @@
-console.log('Hello, front end');
+
+document.addEventListener('DOMContentLoaded', function() {
+    // var button = document.createElement("buton");
+    // button.innerHTML = "Submit";
+    
+    // var body = document.getElementsByTagName("body")[0];
+    // body.appendChild
+    
+    document.getElementById("subBtn").addEventListener("click", function(event) {
+        event.preventDefault();
+        const go = document.querySelector("#inputTemperature").value;
+        document.getElementById("outputTemperature").innerHTML = Math.round(celsiusConverter(go));
+        
+    })
+
+
+    
+    function celsiusConverter(f) {
+        return (5/9) * (f-32);
+    
+    }
+    function fahrenConverter(c) {
+        return (9/5) * (c+32);
+    }
+    
+    
+
+
+})
+
