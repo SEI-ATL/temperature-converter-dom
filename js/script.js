7514f1a310222b8bbb32a9004431e0a1ca3b9004
Tu Hoa Huynh <tuhoa.huynh@yahoo.com>
2020-10-22 22:15:26 -0400

    [js] Add js code

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
-console.log('Hello, front end');
+document.getElementById('convert').addEventListener('click', function(){
+    var x = document.getElementById('tempToBeConverted').value;
+    var y = (x -32) * (5/9);
+    var z = (y.toFixed(2));
+    if (z > 24) {
+        document.querySelector('#result').style.backgroundColor = 'red';
+    } else {
+        document.querySelector('#result').style.backgroundColor = 'blue';
+    }
+    document.querySelector('#result').innerHTML = `${z} °C`;
+})
+
+
+document.getElementById('clear').addEventListener('click', function(){
+    document.querySelector('#tempToBeConverted').value = null;
+    document.querySelector('#result').innerHTML = "";
+}
+)
