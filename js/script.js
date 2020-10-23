97cb67b2204d3b7855653a46de1bbdd40223fcd6
Nicole Hamilton <Nicole@Warps-MacBook-Air.local>
2020-10-22 17:10:49 -0700

    [css js] final script for clear button commit and some css styling

--- js/script.js
+++ js/script.js
@@ -1,6 +1,4 @@
-// const fahrenheit = document.querySelector('#input').value
-
-// // document.querySelector('#here').value What I used to try to take the place of 0
+// document.querySelector('#here').value What I used to try to take the place of 0
 // const celsius = 0
 
 // function fahToCel() {
@@ -15,15 +13,21 @@
 // }
 
 
-const output =document.querySelector('#submit').addEventListener('click', function() {
-    const fahrenheit = document.querySelector('#input').value
+let output = document.querySelector('#submit').addEventListener('click', function() {
+    let fahrenheit = document.querySelector('#input').value;
     console.log(fahrenheit);
     let cel = ((fahrenheit - 32)/1.8)
     console.log(cel);
-    return cel
-    
+    document.querySelector('.output').innerHTML = `${cel}C`
+
 })
-console.log(output);
+
+let clear = document.querySelector('#clear').addEventListener('click', function() {
+    document.querySelector('.output').innerHTML = " "
+    // document.getElementById("clear").reset();
+})
+
+
 
 //on button click run function fahToCel grabbing info from input GOAL
 //clear Button should set the output to 0 or null
