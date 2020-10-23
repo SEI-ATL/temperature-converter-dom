54ad3a8c546f7323849a101675f4de9904aa3bcc
swolepenguin <ashtonm223@gmail.com>
2020-10-22 22:38:48 -0400

    my solo attempt

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
-console.log('Hello, front end');
+function convertToCelsius(Fahrenheit) {
+    return (5/9) * (Fahrenheit - 32);
+}
+
+function convertToFahrenheit(Celsius) {
+    return (9/5) * Celsius + 32; 
+}
+//console.log(convertToCelsius(2));
+//console.log(convertToFahrenheit(3));
+document.querySelector('.sub').addEventListener('click',function(e){
+    e.preventDefault()
+    const temperature = document.querySelector('#temperature').value
+    document.getElementById('output').innerHTML = convertToCelsius(temperature)
+})
+
