a8c0e64e7cb6157368accd55597c8e3a5a9f69c2
zfinnan <63361320+zfinnan@users.noreply.github.com>
2020-10-22 20:45:21 -0400

    Making the Converter

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
-console.log('Hello, front end');
+const convertFahrenheit = () => {
+    const f = document.getElementById('f').value;
+    let newC = (f - 32) * 5/9;
+    console.log(newC);
+    document.getElementById('c').value = newC;
+}
+
+const convertCelsius = () => {
+    const cOne = document.getElementById('cOne').value;
+    let newF = (cOne * 9/5) + 32;
+    console.log(newF);
+    document.getElementById('fOne').value = newF;
+}
+
+const reset = () => {
+    document.getElementById('f').reset();
+}
